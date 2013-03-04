cefd274f251dc555337b4fe859dd8ee0c17cfe85
Anthony <anthonycvella@gmail.com>
2013-03-04 13:39:14 -0500

    Removed HomeTableViewController

--- Hiskor/CheckInViewController.h
+++ Hiskor/CheckInViewController.h
@@ -10,10 +10,17 @@
 #import "ZBarReaderViewController.h"
 #import "NetworkingManager.h"
 
-@interface CheckInViewController : UIViewController <ZBarReaderDelegate, NetworkingResponseHandler>
+@interface CheckInViewController : UITableViewController <ZBarReaderDelegate, NetworkingResponseHandler>
+- (IBAction)refreshGames:(id)sender;
+- (IBAction)btnLogout:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *homeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *awayLabel;
 
+- (void) loadGames;
 - (void) checkTicketForString:(NSString *)ticketCode;
 
+@property (nonatomic, assign) BOOL animateBOOL;
+@property (nonatomic, strong) NSArray *games;
 @property (nonatomic, strong) NSDictionary *JSONResponse;
 @property (nonatomic, strong) NSDictionary *gameData;
 @property (nonatomic, retain) IBOutlet UITextView *resultText;
--- Hiskor/CheckInViewController.m
+++ Hiskor/CheckInViewController.m
@@ -9,13 +9,19 @@
 #import "CheckInViewController.h"
 #import "ZBarReaderViewController.h"
 #import "ZBarReaderView.h"
+#import "TabBarViewController.h"
+#import "Lockbox.h"
+#import "GamesTableViewCell.h"
+
+#define kUserIDKeyString          @"UserIDKeyString"
+#define kLoggedinStatusKeyString    @"LoggedinStatusKeyString"
 
 @interface CheckInViewController ()
 
 @end
 
 @implementation CheckInViewController
-@synthesize JSONResponse, gameData, resultText;
+@synthesize JSONResponse, gameData, resultText, animateBOOL, games;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -31,13 +37,6 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     //NSLog(@"Game Data: %@", gameData);
-    
-    ZBarReaderViewController *reader = [ZBarReaderViewController new];
-    reader.readerDelegate = self;
-    ZBarImageScanner *scanner = reader.scanner;
-    [scanner setSymbology: ZBAR_QRCODE config: ZBAR_CFG_ENABLE to: 1];
-    [self presentModalViewController: reader animated: YES];
-
 }
 
 - (void)didReceiveMemoryWarning
@@ -46,21 +45,18 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void) imagePickerController: (UIImagePickerController*) reader didFinishPickingMediaWithInfo: (NSDictionary*) info {
-    // ADD: get the decode results
-    id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
-    ZBarSymbol *symbol = nil;
-    for(symbol in results)
-        // EXAMPLE: just grab the first barcode
-        break;
+- (void)loadGames {
     
-    // EXAMPLE: do something useful with the barcode data
-    resultText.text = symbol.data;
-    [self checkTicketForString:symbol.data];
-
+    NSString *userID = [Lockbox stringForKey:kUserIDKeyString];
+    NSString *type = @"game";
     
-    // ADD: dismiss the controller (NB dismiss from the *reader*!)
-    [reader dismissModalViewControllerAnimated: YES];
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            userID, @"userID",
+                            type, @"type",
+                            nil];
+    
+	[NetworkingManager sendDictionary:params responseHandler:self];
+    
 }
 
 - (void)checkTicketForString:(NSString *)ticketCode {
@@ -78,23 +74,125 @@
     
 }
 
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [games count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    GamesTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    NSDictionary *gamesDictionary = [games objectAtIndex:indexPath.row];
+    
+    [[cell homeLabel] setText:[gamesDictionary objectForKey:@"homeSchool"]];
+    [[cell awayLabel] setText:[gamesDictionary objectForKey:@"awaySchool"]];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //[self performSegueWithIdentifier:@"checkIn" sender:indexPath];
+    
+    ZBarReaderViewController *reader = [ZBarReaderViewController new];
+    reader.readerDelegate = self;
+    ZBarImageScanner *scanner = reader.scanner;
+    [scanner setSymbology: ZBAR_QRCODE config: ZBAR_CFG_ENABLE to: 1];
+    
+    NSDictionary *game = [games objectAtIndex:indexPath.row];
+    gameData = game;
+
+    [self presentModalViewController: reader animated: YES];
+
+}
+
+/*- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
+    if ([segue.identifier isEqualToString:@"checkIn"]){
+        CheckInViewController *vc = [segue destinationViewController];
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        NSDictionary *game = [games objectAtIndex:indexPath.row];
+        vc.gameData = game;
+    }
+}*/
+
+- (void) imagePickerController: (UIImagePickerController*) reader didFinishPickingMediaWithInfo: (NSDictionary*) info {
+    // ADD: get the decode results
+    id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
+    ZBarSymbol *symbol = nil;
+    for(symbol in results)
+        // EXAMPLE: just grab the first barcode
+        break;
+    
+    // EXAMPLE: do something useful with the barcode data
+    resultText.text = symbol.data;
+    [self checkTicketForString:symbol.data];
+    
+    
+    // ADD: dismiss the controller (NB dismiss from the *reader*!)
+    //[reader dismissModalViewControllerAnimated: YES];
+}
+
 - (void)networkingResponseReceived:(id)response ForMessage:(NSDictionary *)message {
-	
-	if ([[response valueForKeyPath:@"message"] isEqualToString:@"Failed"]) {
-		
-		UIAlertView *loginAlert = [[UIAlertView alloc] initWithTitle:@"Game pull failed" message:@"An error has occured pulling games" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
-		
-		[loginAlert show];
-		
-	} else {
-		NSLog(@"JSON Response: %@", response);
-	}
+    
+    // Filters the type of response, checks if response is game
+    if ([[message valueForKeyPath:@"type"] isEqualToString:@"game"]) {
+        
+        if ([[response valueForKeyPath:@"message"] isEqualToString:@"Failed"]) {
+            
+            UIAlertView *loginAlert = [[UIAlertView alloc] initWithTitle:@"Game pull failed" message:@"An error has occured pulling games" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            
+            [loginAlert show];
+            
+        } else {
+            NSLog(@"JSON Response: %@", response);
+            JSONResponse = [response copy];
+            games = [JSONResponse objectForKey:@"games"];
+            [self.tableView reloadData];
+        }
+    
+    // Checks if response is checkTicket
+    } else if ([[message valueForKeyPath:@"type"] isEqualToString:@"checkTicket"]) {
+        
+        if ([[response valueForKeyPath:@"message"] isEqualToString:@"Failed"]) {
+            
+            UIAlertView *loginAlert = [[UIAlertView alloc] initWithTitle:@"Error Scanning" message:@"An error has occured scanning ticket" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            
+            [loginAlert show];
+            
+        } else {
+            NSLog(@"JSON Response: %@", response);
+        }
+    }
 }
 
 - (void)networkingResponseFailedForMessage:(NSDictionary *)message error:(NSError *)error {
 	
 	NSLog(@"Error with request");
 	NSLog(@"%@", [error localizedDescription]);
+}
+
+- (IBAction)refreshGames:(id)sender {
+    [self loadGames];
+}
+
+- (IBAction)btnLogout:(id)sender {
+    
+    self.animateBOOL = YES;
+    [Lockbox setString:@"FALSE" forKey:kLoggedinStatusKeyString];
+    [(TabBarViewController *)[self tabBarController] loginCheck:self.animateBOOL];
 }
 
 @end
